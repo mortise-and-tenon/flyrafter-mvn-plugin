4087a6a5a9a06d4713e560fa406919d30ec2c16e
Moon Wu <softengineerwu@gmail.com>
2021-11-08 09:44:32 +0000

    !3 适配0.0.6 * 适配新版本flyrafter

---
+++ demo/flyrafterdemo/src/main/java/com/example/demo/entity/EntityParent2.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+
+import fun.mortnon.flyrafter.annotation.Ignore;
+
+/**
+ * @author Moon Wu
+ * @date 2021/11/8
+ */
+public class EntityParent2 {
+    private String parentS1;
+
+    @Ignore
+    private String parentS2;
+
+    private String parentS3;
+}
